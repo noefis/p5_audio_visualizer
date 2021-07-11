49eabffeef014acafae3d9e0fdfa612040683b8a
Noel Fischer <noefis>
2021-07-11 13:28:55 +0200

    colorful doublebars

--- visualizers/colorful_doublebars/sketch.js
+++ visualizers/colorful_doublebars/sketch.js
@@ -30,7 +30,11 @@ function draw() {
     var amp = spectrum[i];
     let x = -ww/2 + (wh/l-3)/2+6 + i * ww/(l+1);
     let y = -amp*Math.pow(i+1, 1/(pow/2))*wh/456 + wh/2;
-    stroke(255);
-    rect(x, wh/2 - 20, wh/l-3, y > 0 ? 0: y);
+    let z = (y > 0 ? 0: abs(y/6%360));
+    let c = color(abs(z-31%360), 100, z);
+    stroke(c);
+    fill(c);
+    rect(x, 0, wh/l-3, y > 0 ? 0: y/2);
+    rect(x, 0, wh/l-3, y > 0 ? 0: -y/2);
   }
 }
