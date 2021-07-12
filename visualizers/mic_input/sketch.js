73e69a199e85514f94ee762593d9fcb34f77a9c0
noefis <59683264+noefis@users.noreply.github.com>
2021-07-12 16:26:54 +0200

    make it functional

--- visualizers/mic_input/sketch.js
+++ visualizers/mic_input/sketch.js
@@ -1,4 +1,4 @@
-let song;
+let mic;
 let fft;
 let button;
 
@@ -6,31 +6,31 @@ let ww = window.innerWidth;
 let wh = window.innerHeight;
 let pow = 12;
 
-
-function preload() {
-  song = new p5.AudioIn();
-}
-
 function setup() {
   createCanvas(ww, wh);
   colorMode(HSB);
-  button = createButton('toggle');
-  button.mousePressed(() => song.isPlaying() ? song.pause() : song.play());
-  song.play();
+
+  mic = new p5.AudioIn();
+  mic.start();
+
   fft = new p5.FFT(0.9, Math.pow(2, pow));
+  fft.setInput(mic);
 }
 
 function draw() {
   background(0);
   let spectrum = fft.analyze();
-  let l = spectrum.length/3;
+  let l = spectrum.length / 3;
+
+
   noStroke();
   translate(width / 2, height / 2);
   for (var i = 0; i < l; i++) {
-    var amp = spectrum[i];
-    let x = -ww/2 + (wh/l-3)/2+6 + i * ww/(l+1);
-    let y = -amp*Math.pow(i+1, 1/(pow/2))*wh/456 + wh/2;
+    var amp = spectrum[i] * 2.1;
+
+    let x = -ww / 2 + (wh / l - 3) / 2 + 6 + i * ww / (l + 1);
+    let y = -amp * Math.pow(i + 1, 1 / (pow / 2)) * wh / 456 + wh / 2;
     stroke(255);
-    rect(x, wh/2 - 20, wh/l-3, y > 0 ? 0: y);
+    rect(x, wh / 2 - 20, wh / l - 3, y > 0 ? 0 : y);
   }
 }
